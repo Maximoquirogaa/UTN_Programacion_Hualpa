9d1c26fe7fe0fe1f382783545e0c0be2963a0316
Máximo <maximoquirogajc@gmail.com>
2025-09-12 22:17:08 -0300

    Subida trabajo en clase 11 septiembre

---
+++ Trabajos_clase/11_septiembre/alumnos.py
@@ -0,0 +1,62 @@
+import random
+
+alumnos = [
+    "Valentina De Los ángeles Albizú",
+    "Pablo Andres Allende",
+    "Luca Valentín Argumedo",
+    "Pablo Avalos",
+    "Lucas Avila",
+    "Santino Barone",
+    "Sofia Blangetti",
+    "Nicolás Bohm",
+    "Renzo Valentino Borello Canizo",
+    "Juan Manuel Carrillo Taglio",
+    "Facundo Gustavo Chacon Catalan",
+    "Agustin Emiliano Contardi",
+    "Jeronimo Coronel Alvarez",
+    "Gabriel Emiliano Denis",
+    "Facundo Gustavo Deseff",
+    "Juan Martin García",
+    "Enzo Giaquinta",
+    "Sabrina Gimenez",
+    "Joaquin Godoy",
+    "Lucas Facundo Godoy",
+    "Santino Alejo Godoy Galdeano",
+    "Valentina Antonela Gordillo Moreno",
+    "Lautaro Agustin Guardatti Esquivel",
+    "Tiago Nahuel Guillot Duran",
+    "Mateo Lautaro Liendo",
+    "Juan Ignacio Martinez Quiroga",
+    "Maximo Exequiel Monardez",
+    "Tomas Agustin Mora Gonzalez",
+    "Pablo Isaias Morinigo Lima",
+    "Ares Martín Ocaña Martinez",
+    "Thiago Santino Oviedo Saldaño",
+    "Amanda Lucrecia Pagano",
+    "Máximo Juan Cruz Quiroga",
+    "Facundo Agustín Ramírez García",
+    "Franco Agustin Rios Alzamora",
+    "Leonel Enrique Rojas",
+    "Matias Nicolas Ruiz Vargas",
+    "Ramiro Ezequiel Salcedo",
+    "Ismael Saleme Padolsky",
+    "Ignacio Exequiel Sanchez",
+    "Fabrizio Jonathan Simon Santos",
+    "Cristian Gabriel Soto",
+    "Giovanna Mercedes Suarez",
+    "Ismael Mauricio Suarez",
+    "Fernando Agustín Torrez",
+    "Luca Nicolas Valdez",
+    "Tiziano Ignacio Valentini",
+    "Matias Nicolas Vargas",
+    "Juan Ignacio Videla Continella",
+    "Pablo Exequiel Avalos"
+]
+
+numeros = random.sample(range(len(alumnos)),(len(alumnos)))
+alumnos_mezclados = [alumnos[i] for i in numeros]
+alumnos_matriz = [alumnos_mezclados [ _:(_+4)] for _ in range(0,len(alumnos_mezclados),4)]
+
+
+for fila in alumnos_matriz:
+    print(fila)
